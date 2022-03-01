b5546be13572523183ed28a10276394a2c9aac86
Dariusz Okonski <darek200180@gmail.com>
2022-03-01 15:03:29 +0000

    add event to input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = '03-optional';
+export class AppComponent {  
+  randomText = lorem.sentence();
+
+  onInput(event: any) {
+    const value = event.target.value;
+
+    console.log(value)
+  }
 }
